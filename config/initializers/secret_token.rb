14283516904931a804bd19e37e40577340b6c0b0
JuanCarlos <juanjcsr@gmail.com>
2013-09-24 13:34:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+AcopiaMe::Application.config.secret_key_base = '343ed7305ea515bd1cdb56717218461d2bb1e5c3c0e35e3aae1005095c40592060fd966f33dfc775a2ac6275292d9c95d8abb066bad746b0fa0c918607a0a50c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AcopiaMe::Application.config.session_store :cookie_store, key: '_acopia_me_session'
